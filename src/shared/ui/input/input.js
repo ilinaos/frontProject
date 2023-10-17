6b045fa3c2985585d427ef6ed03b14696cd20b88
ilinaos <ilinaos@ya.ru>
2023-10-17 14:44:06 +0500

    добавление ui-элементов + исправление ошибок

--- src/shared/ui/input/input.js
+++ src/shared/ui/input/input.js
@@ -1,6 +1,6 @@
 export default () => {
     return `
-    <input type="text" className="page__input" placeholder="Ваш текст"></input>
+    <input type="text" className="page__input" placeholder="Ваш текст">
 <script >
 let input = document.querySelector(".page__input")
 input.addEventListener("input", function() {
